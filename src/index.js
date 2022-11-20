2b0d037a9fa98eba71007273b37c8167c864e71f
drag13 <no-questions@github.noreply.com>
2022-11-20 17:25:34 +0200

    tuned errors

--- src/index.js
+++ src/index.js
@@ -8,21 +8,23 @@ app.use(cors());
 const port = process.env.PORT ?? 3000;
 
 app.get("/course", (_, res) => {
+  const rnd = rnd10();
+
+  if (rnd < 4) {
+    return res.status(500).send("Something went wrong");
+  }
+
   const maybeCourse = getDrunkenCourse();
   res.send(maybeCourse);
 });
 
 function getDrunkenCourse() {
   const random10 = rnd10();
-  if (random10 < 1) {
+  if (random10 < 4) {
     return null;
   }
 
-  if (random10 < 2) {
-    return [];
-  }
-
-  if (random10 < 5) {
+  if (random10 < 7) {
     return {
       title: courseRaw.title,
       lessons: null,
