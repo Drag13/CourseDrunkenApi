e956af7579c4b515a90da99c317d01a2069c210f
drag13 <no-questions@github.noreply.com>
2022-11-20 19:50:41 +0200

    Added new route lesson/name

--- src/index.js
+++ src/index.js
@@ -18,6 +18,31 @@ app.get("/course", (_, res) => {
   res.send(maybeCourse);
 });
 
+const matchLesson = ({ title, name }, key) => {
+  if (!key || typeof key !== "string") return lesson;
+  const search = key.toLocaleLowerCase().trim();
+  return (
+    title.toLocaleLowerCase().includes(search) ||
+    name.toLocaleLowerCase().includes(search)
+  );
+};
+
+app.get("/lesson/:name", (req, res) => {
+  const rnd = rnd10();
+
+  if (rnd < 4) {
+    return res.status(500).send("Something went wrong");
+  }
+
+  const lessonName = req.params.name ?? "";
+
+  const lesson = courseRaw.lessons.filter((lesson) =>
+    matchLesson(lesson, lessonName)
+  );
+
+  res.send(lesson);
+});
+
 function getDrunkenCourse() {
   const random10 = rnd10();
   if (random10 < 4) {
