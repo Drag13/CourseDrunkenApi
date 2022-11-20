16eb1ae785906707112b8eccfd2c42d9dfe08948
drag13 <no-questions@github.noreply.com>
2022-11-20 16:01:35 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const courseRaw = require("./course.json");
+const { rnd10 } = require("./rnd");
+const app = express();
+
+const port = process.env.PORT ?? 3000;
+
+app.get("/course", (_, res) => {
+  const maybeCourse = getDrunkenCourse();
+  res.send(maybeCourse);
+});
+
+function getDrunkenCourse() {
+  const random10 = rnd10();
+  if (random10 < 1) {
+    return null;
+  }
+
+  if (random10 < 2) {
+    return [];
+  }
+
+  if (random10 < 5) {
+    return {
+      title: courseRaw.title,
+      lessons: null,
+    };
+  }
+
+  return courseRaw;
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ src/rnd.js
@@ -0,0 +1,3 @@
+const rnd10 = () => Math.floor(Math.random(Date.now()) * 10);
+
+module.exports = { rnd10 };
