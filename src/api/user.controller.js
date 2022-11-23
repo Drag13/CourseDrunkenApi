bdba293290ad294ee51fdba7e2807f09a89e0946
drag13 <no-questions@github.noreply.com>
2022-11-23 11:46:13 +0200

    Added new endpoint for the user

---
+++ src/api/user.controller.js
@@ -0,0 +1,42 @@
+const { rnd10 } = require("../helpers/math");
+const joi = require("joi");
+
+const userSchema = joi.object({
+  name: joi.string().required().min(4),
+  password: joi.string().required().min(6),
+  passwordConfirmation: joi.string().equal(joi.ref("password")).required(),
+});
+
+function createUserHandler(req, res) {
+  const maybeUser = req.body;
+
+  const validationResult = userSchema.validate(maybeUser, {
+    abortEarly: false,
+  });
+
+  if (validationResult.error) {
+    const errors = validationResult.error.details.map(({ message }) => ({
+      error: message,
+    }));
+
+    return res.status(400).send(errors);
+  }
+
+  const rnd = rnd10();
+
+  if (rnd < 3) {
+    return res.status(500).send("Something went wrong");
+  }
+
+  return res.send({ scoring: "SS" });
+}
+
+module.exports = {
+  createUserHandler,
+};
+
+/**
+fetch('http://localhost:3000/user', {body: JSON.stringify({name: 'test', password: '123123', passwordConfirmation: '123123'}), method: 'POST',   headers: {
+      'Content-Type': 'application/json'
+    }}).then(x=>x.json()).then(console.log);
+ */
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+
 const {
   courseHandler,
   searchLessonHandler,
 } = require("./api/course.controller");
 
+const { createUserHandler } = require("./api/user.controller");
+
 app.use(cors());
+app.use(bodyParser.json());
 
 const port = process.env.PORT ?? 3000;
 
 app.get("/course", courseHandler);
 app.get("/lesson/:name", searchLessonHandler);
+app.post("/user", createUserHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
